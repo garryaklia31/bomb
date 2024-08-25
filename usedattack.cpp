78e5b8732521564c89d44d3906e652d5274721b2
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2024-08-25 11:11:26 +0530

    Add files via upload

---
+++ usedattack.cpp
@@ -0,0 +1,6 @@
+UserID: 1718738592 | Time: 2024-08-25 11:05:35.769707 | Command: /bgmi | Target: 20.193.140.138 | Port: 8700 | Time: 60
+Username: @Sidhu_garry
+Target: 20.193.140.138
+Port: 8700
+Time: 60
+
